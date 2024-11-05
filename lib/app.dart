1536763106e4902ae8c6d0f785ad5b81242f6bcc
netos23 <nikmoroz2002@outlook.com>
2024-11-05 15:01:00 +0300

    feature(app): add localisations

---
+++ lib/app.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:pivo_front/res/theme.dart';
+import 'package:pivo_front/res/util.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final brightness = View.of(context).platformDispatcher.platformBrightness;
+
+    TextTheme textTheme = createTextTheme(context, "Montserrat", "Montserrat");
+
+    MaterialTheme theme = MaterialTheme(textTheme);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: brightness == Brightness.light ? theme.light() : theme.dark(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ru'),
+      ],
+      home: const SizedBox(),
+    );
+  }
+}
