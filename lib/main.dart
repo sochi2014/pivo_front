d0726a6b575318f72be69fe4235ada591e65ae6d
netos23 <nikmoroz2002@outlook.com>
2024-11-05 15:35:56 +0300

    feature(app): add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pivo_front/app.dart';
 
+import 'firebase_options.dart';
+
 void main() {
-  runZonedGuarded(() {
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
     runApp(const App());
   }, (error, stackTrace) {
     debugPrintStack(stackTrace: stackTrace);
