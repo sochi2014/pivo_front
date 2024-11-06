a6779e6abbf220fe284ca492a115db989183d5c6
netos23 <nikmoroz2002@outlook.com>
2024-11-06 15:35:59 +0300

    feature(map): add maps integration

--- lib/pages/map_page/map_page_widget.dart
+++ lib/pages/map_page/map_page_widget.dart
@@ -1,10 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:elementary/elementary.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pivo_front/pages/map_page/maps/mobile_map.dart';
+import 'package:pivo_front/pages/map_page/maps/web_map.dart';
 
 import 'map_page_wm.dart';
-import 'maps/web_map.dart';
 
 // TODO: cover with documentation
 /// Main widget for MapPage module
@@ -17,7 +18,14 @@ class MapPageWidget extends ElementaryWidget<IMapPageWidgetModel> {
 
   @override
   Widget build(IMapPageWidgetModel wm) {
-    return MobileMap(
+    if (defaultTargetPlatform == TargetPlatform.iOS ||
+        defaultTargetPlatform == TargetPlatform.android) {
+      return MobileMap(
+        key: mapKey,
+      );
+    }
+
+    return WebMap(
       key: mapKey,
     );
   }
